89ea6cf2b70441ceeb8ad441d7fcc01bdc1c760a
Aloblanko <125378394+Aloblanko@users.noreply.github.com>
2023-12-14 03:21:21 +0100

    jwt-halftime

--- routes/user.route.js
+++ routes/user.route.js
@@ -7,6 +7,8 @@ import { emailvalidation } from "../middlewares/validate/email.validate.js";
 import { validationHandler } from "../middlewares/validate/validation.js";
 import { namevalidation } from "../middlewares/validate/name.validate.js";
 import { passwordvalidation } from "../middlewares/validate/passwort.validate.js";
+import pkg from 'jsonwebtoken';
+const { jwt } = pkg;
 
 export const userRouter = Router();
 
@@ -20,10 +22,21 @@ userRouter.post(
 );
 
 userRouter.get("/", (req, res, next) => {
-    const count = req.cookies.count;
+
+     const count = req.cookies.count; // warum ist das im code?
+
+   // const secretKey = 'deinGeheimerSchlüssel';
+
+//  const user = {
+//     id: '123',
+//       name: 'Beispielbenutzer'
+//  };
+
+    const token = jwt.sign(user, secretKey, { expiresIn: '1h' });
+
     res
         .status(200)
-        .cookie("jwt", jwt, {
+        .cookie("jwt", token, {
             expires: new Date(Date.now() + 3600000),
             httpOnly: true,
         })
